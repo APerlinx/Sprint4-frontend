9d7feb06e94e58f9a0b6575b9a443108d892916c
Guy Dahan <guy4191@gmail.com>
2023-07-25 20:42:30 +0300

    basic task details

---
+++ src/views/TaskDetails.vue
@@ -0,0 +1,37 @@
+<template>
+    <section class="task-details">
+        <section class="task-details-header">
+            <input type="text" class="task-details-title" placeholder='v-model="taskToEdit.title"'>check
+            <!-- v-model="taskToEdit.title" -->
+            <!-- {{ taskToEdit.title }} -->
+            <p>In list 'need to add list.name'</p>
+
+        </section>
+
+        <section class="task-detail-main">
+            <div>
+                <p>Notifications</p>
+                <button>watch</button>
+            </div>
+
+            <div>
+                <h3>Description</h3>
+                <textarea placeholder="Add a more detailed description..." class="description-textarea">
+
+                </textarea>
+            </div>
+            <!-- v-model="taskToEdit.description" -->
+        </section>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+}
+
+</script>
