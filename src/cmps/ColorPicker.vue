5f3f74d2fffa132c8f5cbd1c2e6b48316d08ff16
Shay Zigdon <zigshay@gmail.com>
2023-07-27 20:09:05 +0300

    add board cmp done pixel perfect, all func

---
+++ src/cmps/ColorPicker.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="color-pallte">
+    <div
+      v-for="(color, index) in colorOptions"
+      :key="index"
+      @click="setBgColor(color)"
+      :class="colorClass[index]"
+    ></div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      colorOptions: [
+        //green
+        "linear-gradient(328deg, rgba(9, 55, 118, 1) 0%, rgba(159, 226, 17, 1) 100%)", 
+        
+        //blue
+            "linear-gradient(328deg, rgba(9, 55, 118, 1) 0%, rgba(17, 107, 226, 1) 100%)",
+
+        //dark-blue
+
+            "linear-gradient(328deg, rgba(11, 97, 216, 1) 0%, rgba(9, 55, 118, 1) 100%)",
+        //dark-purple
+
+            "linear-gradient(328deg, rgba(121, 96, 196, 1) 0%, rgba(25, 52, 111, 1) 100%)",
+        //purple
+
+            "linear-gradient(328deg, rgba(121, 96, 196, 1) 0%, rgba(219, 115, 189, 1) 100%)",
+        //orange
+
+            "linear-gradient(328, rgba(230, 85, 55, 1) 0%, rgba(247, 154, 60, 1) 100%)",
+      ],
+      colorClass: [
+        "green",
+        "blue",
+        "dark-blue",
+        "dark-purple",
+        "purple",
+        "unique",
+      ],
+    };
+  },
+  methods: {
+    setBgColor(color) {
+      this.$emit("setBgColor", color)
+    },
+  },
+};
+</script>
--- src/services/board.service.local.js
+++ src/services/board.service.local.js
@@ -64,10 +64,11 @@ async function addBoardMsg(boardId, txt) {
   return msg
 }
 
-function getEmptyBoard(title = '', imgUrl = 'https://images.unsplash.com/photo-1600691792883-dc29f53f6b17?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80') {
+function getEmptyBoard(title = '', imgUrl = '', bgColor = '') {
   return {
     title,
-    imgUrl
+    imgUrl,
+    bgColor
   }
 }
 function getEmptyGroup(title) {
