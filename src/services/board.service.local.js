da19171fca78ae17c38fbd1098fb77ba273af910
Shay Zigdon <zigshay@gmail.com>
2023-07-29 20:08:32 +0300

    check

--- src/services/board.service.local.js
+++ src/services/board.service.local.js
@@ -196,16 +196,13 @@ const board = {
                 }
               ]
             }
-          ],,
-          "labelIds": [{ id: "l101", checked: false, bgColor: "#bc9609", txt: '' },
-          { id: "l102", checked: true, bgColor: "#bbf3db", txt: '' },
-          { id: "l103", checked: false, bgColor: "#faa63d", txt: '' },
-          { id: "l104", checked: false, bgColor: "#f87562", txt: '' },
-          { id: "l105", checked: false, bgColor: "#9f90ef", txt: '' }],
+          ],
         },
         {
           "id": "c102",
-          "title": "Add Samples"
+          "title": "Add Samples",
+          "description": "Add samples to the data",
+
         }
       ],
       "style": {}
@@ -218,11 +215,6 @@ const board = {
           "id": "c103",
           "title": "Fix bug",
           "archivedAt": 1589983468418,
-          "labelIds": [{ id: "l101", checked: false, bgColor: "#bc9609", txt: '' },
-          { id: "l102", checked: true, bgColor: "#bbf3db", txt: '' },
-          { id: "l103", checked: false, bgColor: "#faa63d", txt: '' },
-          { id: "l104", checked: false, bgColor: "#f87562", txt: '' },
-          { id: "l105", checked: false, bgColor: "#9f90ef", txt: '' }],
           "description": "Fix bug line on moveTask function",
         },
         {
@@ -337,21 +329,11 @@ const board2 = {
       "tasks": [
         {
           "id": "c101",
-          "title": "Eat",
-          "labelIds": [{ id: "l101", checked: false, bgColor: "#bc9609", txt: '' },
-          { id: "l102", checked: true, bgColor: "#bbf3db", txt: '' },
-          { id: "l103", checked: false, bgColor: "#faa63d", txt: '' },
-          { id: "l104", checked: false, bgColor: "#f87562", txt: '' },
-          { id: "l105", checked: false, bgColor: "#9f90ef", txt: '' }],
+          "title": "Eat"
         },
         {
           "id": "c102",
-          "title": "Sleep",
-          "labelIds": [{ id: "l101", checked: false, bgColor: "#bc9609", txt: '' },
-          { id: "l102", checked: true, bgColor: "#bbf3db", txt: '' },
-          { id: "l103", checked: false, bgColor: "#faa63d", txt: '' },
-          { id: "l104", checked: false, bgColor: "#f87562", txt: '' },
-          { id: "l105", checked: false, bgColor: "#9f90ef", txt: '' }],
+          "title": "Sleep"
         }
       ],
       "style": {}
@@ -364,11 +346,6 @@ const board2 = {
           "id": "c103",
           "title": "Do that",
           "archivedAt": 1589983468418,
-          "labelIds": [{ id: "l101", checked: false, bgColor: "#bc9609", txt: '' },
-          { id: "l102", checked: true, bgColor: "#bbf3db", txt: '' },
-          { id: "l103", checked: false, bgColor: "#faa63d", txt: '' },
-          { id: "l104", checked: false, bgColor: "#f87562", txt: '' },
-          { id: "l105", checked: false, bgColor: "#9f90ef", txt: '' }],
         },
         {
           "id": "c104",
@@ -446,76 +423,3 @@ const board2 = {
 //   await storageService.post(STORAGE_KEY, board)
 //   await storageService.post(STORAGE_KEY, board2)
 // })()
-
-function coverOptions() {
-  return {
-    coverColors: ["#277da1", "#4d908e", "#fb6f92", "#90be6d", "#f9c74f", "#f9844a", "#00b4d8", "#3a5a40"],
-    coverImgs: [
-      "https://webneel.com/daily/sites/default/files/images/daily/08-2018/1-nature-photography-spring-season-mumtazshamsee.jpg",
-      "https://media.istockphoto.com/photos/the-sun-goes-down-behind-the-autumn-forest-picture-id1162998855?k=20&m=1162998855&s=612x612&w=0&h=JLbCH4hLaO5war1ipJXx7eoxXMdhcMXFO9pwXz1NR1Q=",
-      "https://wallpaperaccess.com/full/1131217.jpg",
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/close-up-of-tulips-blooming-in-field-royalty-free-image-1584131603.jpg",
-      "https://media.istockphoto.com/photos/hydrangea-flowers-in-the-garden-picture-id927499422?k=20&m=927499422&s=612x612&w=0&h=9fZtBAXZ3I8qNRTi87SHTDEjbBjyn_eRoDoLAC7zVvg=",
-      "https://i.pinimg.com/originals/18/28/4e/18284ec99d85c9ee5afaf05baf77083a.jpg",
-      "https://media.istockphoto.com/photos/colored-ceiling-picture-id1208301897?k=20&m=1208301897&s=612x612&w=0&h=xXFlsJphxez3hgCYRxYmS7yxb5P4-HOtbnsjIVJSSWA=",
-      "https://assets.weforum.org/global_future_council/image/xALg-7b0WN5aLOY6aejbKW3NepG-PEipzKnEuyS8ZlI.jpeg",
-      "https://media.cntraveler.com/photos/5ca60f7f7b531a5e47949cde/master/w_4000,h_2400,c_limit/NYC_GettyImages-500619014.jpg",
-      "https://images.pexels.com/photos/460621/pexels-photo-460621.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    ],
-  }
-}
-
-function _labelOptions() {
-  return [
-    {
-      id: "l101",
-      bgColor: "#e63946",
-      txt: "Urgent",
-    },
-    {
-      id: "l102",
-      bgColor: "#2a9d8f",
-      txt: "Important",
-    },
-    {
-      id: "l103",
-      bgColor: "#e9c46a",
-      txt: "New",
-    },
-    {
-      id: "l104",
-      bgColor: "#48cae4",
-      txt: "Nice to have",
-    },
-    {
-      id: "l106",
-      bgColor: "#adc178",
-      txt: "Delayed",
-    },
-    {
-      id: "l107",
-      bgColor: "#9c89b8",
-      txt: "In progress",
-    },
-    {
-      id: "l108",
-      bgColor: "#0ead69",
-      txt: "Done",
-    },
-    {
-      id: "l109",
-      bgColor: "#16697a",
-      txt: "Do not forget",
-    },
-    {
-      id: "l110",
-      bgColor: "#70e000",
-      txt: "Bug",
-    },
-    {
-      id: "l111",
-      bgColor: "#00a8e8",
-      txt: "Take care today",
-    },
-  ]
-}
