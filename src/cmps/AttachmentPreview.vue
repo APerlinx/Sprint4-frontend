e9abe07de2b8ca40dcc4d930b02bd3d0fdf4bade
Guy Dahan <guy4191@gmail.com>
2023-08-03 12:31:43 +0300

    merge

--- src/cmps/AttachmentPreview.vue
+++ src/cmps/AttachmentPreview.vue
@@ -1,25 +1,28 @@
 <template >
     <li class="attachment-container">
-        <div class="icon-title-container-description">
-            <span class="icon attachment-big"></span>
-            <h3 class="details-title-big">Attachment</h3>
-        </div>
-        <div class="img-container">
-            <img :src="attachment.url" alt />
-        </div>
-        <article class="attachment-details">
-            <h4>{{ attachment.fileName }}</h4>
-            <div class="main-actions">
-                Added
-                <span>{{ createdAt }} &#x2022 </span>
-                <span class="attachment-action">Comment</span> &#x2022
-                <span class="attachment-action" @click="removeAttachment(attachment.id)">Delete</span> &#x2022
-                <span class="attachment-action">Edit</span>
+        <div v-for="(attachment, idx) in attachments" :idx="idx" class="attachment flex align-center"
+            @click="goTo(attachment.link)">
+            <div class="icon-title-container-description">
+                <span class="icon attachment-big"></span>
+                <h3 class="details-title-big">Attachment</h3>
             </div>
-            <div>
-                <span class="attachment-action">Make Cover</span>
+            <div class="img-container">
+                <img :src="attachment.url" alt />
             </div>
-        </article>
+            <article class="attachment-details">
+                <h4>{{ attachment.fileName }}</h4>
+                <div class="main-actions">
+                    Added
+                    <span>{{ createdAt }} &#x2022 </span>
+                    <span class="attachment-action">Comment</span> &#x2022
+                    <span class="attachment-action" @click="removeAttachment(attachment.id)">Delete</span> &#x2022
+                    <span class="attachment-action">Edit</span>
+                </div>
+                <div>
+                    <span class="attachment-action">Make Cover</span>
+                </div>
+            </article>
+        </div>
     </li>
 </template>
 
