dd8a1d2f00c0478a4b4a2e0a96a9d57e3a69c8fe
Guy Dahan <guy4191@gmail.com>
2023-08-06 10:37:52 +0300

    merge

--- src/views/TaskDetails.vue
+++ src/views/TaskDetails.vue
@@ -267,7 +267,7 @@ export default {
         toggleMember(clickedMember) {
 
             const notification = {
-                byUser: this.loggedinUser.fullname,
+                byUser: this.loggedinUser?.fullname,
                 toUser: clickedMember.fullname,
                 createdAt: Date.now(),
                 action: '',
