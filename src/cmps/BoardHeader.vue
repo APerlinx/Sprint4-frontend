f119138b1cfa4fdcd6fdf38681161d94bd29795a
APerlinx <133282938+APerlinx@users.noreply.github.com>
2023-08-06 09:24:36 +0300

    Board responsive to menu, penicl icon pos

--- src/cmps/BoardHeader.vue
+++ src/cmps/BoardHeader.vue
@@ -237,7 +237,7 @@ export default {
       return this.isStarred ? 'full-star' : 'star'
     },
     headerStyle() {
-      return this.menuOpen ? { width: '1251px' } : {}
+      return this.menuOpen ? { width: '1485px' } : {}
     },
   },
   watch: {
